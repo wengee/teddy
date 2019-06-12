d5796d50d73af56147b5978afde33d6a6bb3c72e
Fung Wing Kit <fengyongjie@vivo.com>
2019-06-12 16:38:48 +0800

    commit

--- src/Validation/Validators/Regex.php
+++ src/Validation/Validators/Regex.php
@@ -1,19 +1,24 @@
 <?php
 /**
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2019-06-06 17:11:38 +0800
+ * @version  2019-06-12 16:36:46 +0800
  */
 namespace Teddy\Validation\Validators;
+
+use Closure;
 
 class Regex extends ValidatorRuleBase
 {
     protected $pattern;
 
+    protected $replacement = null;
+
     protected $message = ':label不符合指定规则';
 
-    public function __construct(string $pattern, ?string $message = null)
+    public function __construct(string $pattern, $replacement = null, ?string $message = null)
     {
         $this->pattern = $pattern;
+        $this->replacement = $replacement;
         $this->message = $message ?: $this->message;
     }
 
@@ -22,6 +27,14 @@ class Regex extends ValidatorRuleBase
         $value = strval($value);
         if (!preg_match($this->pattern, $value)) {
             $this->throwMessage();
+        }
+
+        if ($this->replacement) {
+            if ($this->replacement instanceof Closure) {
+                $value = preg_replace_callback($this->pattern, $this->replacement, $value);
+            } elseif (is_string($this->replacement)) {
+                $value = preg_replace($this->pattern, $this->replacement, $value);
+            }
         }
 
         return $next($value, $data);
