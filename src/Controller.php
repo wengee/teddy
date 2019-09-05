f6d9b14a22ead1171cb3b3930290318ecb3e3242
Fung Wing Kit <fengyongjie@vivo.com>
2019-09-05 15:14:54 +0800

    commit

--- src/Controller.php
+++ src/Controller.php
@@ -3,7 +3,7 @@
  * This file is part of Teddy Framework.
  *
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2019-08-15 10:31:42 +0800
+ * @version  2019-09-05 15:14:47 +0800
  */
 
 namespace Teddy;
@@ -12,6 +12,9 @@ abstract class Controller
 {
     final public function __construct()
     {
+        if (method_exists($this, 'initialize')) {
+            $this->initialize();
+        }
     }
 
     final public function __get($name)
