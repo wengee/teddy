9720520b326a68b3724f8e9977c2a9bc700478cd
Fung Wing Kit <fengyongjie@vivo.com>
2019-10-07 16:41:47 +0800

    commit

--- src/Jwt/Authentication.php
+++ src/Jwt/Authentication.php
@@ -3,7 +3,7 @@
  * This file is part of Teddy Framework.
  *
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2019-08-15 10:31:42 +0800
+ * @version  2019-10-07 16:41:39 +0800
  */
 
 namespace Teddy\Jwt;
@@ -18,13 +18,9 @@ class Authentication implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $jwt = app('jwt');
-        if ($jwt) {
-            try {
-                $request = $jwt->processRequest($request);
-            } catch (Exception $e) {
-                log_exception($e);
-            }
+        try {
+            $request = app('jwt')->processRequest($request);
+        } catch (Exception $e) {
         }
 
         return $handler->handle($request);
