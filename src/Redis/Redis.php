cebcd8e77ce45673e48682adf6d68871f7473663
Fung Wing Kit <fengyongjie@vivo.com>
2019-10-09 17:00:23 +0800

    commit

--- src/Redis/Redis.php
+++ src/Redis/Redis.php
@@ -3,7 +3,7 @@
  * This file is part of Teddy Framework.
  *
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2019-08-15 10:31:42 +0800
+ * @version  2019-10-09 16:50:18 +0800
  */
 
 namespace Teddy\Redis;
@@ -54,9 +54,8 @@ class Redis extends Pool
             'port'      => array_get($config, 'port', 6379),
             'password'  => array_get($config, 'password', ''),
             'dbIndex'   => array_get($config, 'dbIndex', 0),
-            'prefix'   => array_get($config, 'prefix', ''),
+            'prefix'    => array_get($config, 'prefix', ''),
         ];
-
 
         if (is_array($defaultConf['host'])) {
             foreach ($defaultConf['host'] as $host) {
