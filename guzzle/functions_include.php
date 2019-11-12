a72e06f4cec14900c24a9e41407d3b866413e798
Fung Wing Kit <fengyongjie@vivo.com>
2019-11-12 10:54:56 +0800

    add guzzle hook

--- guzzle/functions_include.php
+++ guzzle/functions_include.php
@@ -1,6 +1,11 @@
-<?php
+<?php declare(strict_types=1);
+/**
+ * This file is part of Teddy Framework.
+ *
+ * @author   Fung Wing Kit <wengee@gmail.com>
+ * @version  2019-11-12 10:54:26 +0800
+ */
 
-// 判断环境
 if (!class_exists('GuzzleHttp\Client') || !class_exists('Swoole\Runtime')) {
     return;
 }
@@ -13,10 +18,9 @@ array_pop($ver);
 $ver = implode('.', $ver);
 $dir = "/guzzle-{$ver}";
 
-require __DIR__ . "/functions.php";
+require __DIR__ . '/functions.php';
 if (is_dir(__DIR__ . "/../vendor/mix/guzzle-hook/src/{$dir}")) {
     require __DIR__ . "/../vendor/mix/guzzle-hook/src/{$dir}/functions.php";
 } elseif (is_dir(__DIR__ . "/../../../mix/guzzle-hook/src/{$dir}")) {
     require __DIR__ . "/../../../mix/guzzle-hook/src/{$dir}/functions.php";
 }
-
