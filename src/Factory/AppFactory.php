a9b55a38645a7b4a8974e9af9804ebbd892957fa
Fung Wing Kit <fengyongjie@vivo.com>
2019-12-20 10:12:38 +0800

    commit

--- src/Factory/AppFactory.php
+++ src/Factory/AppFactory.php
@@ -3,19 +3,20 @@
  * This file is part of Teddy Framework.
  *
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2019-12-19 16:28:03 +0800
+ * @version  2019-12-20 10:11:39 +0800
  */
 
 namespace Teddy\Factory;
 
-use Teddy\Swoole\App as SwooleApp;
 use Teddy\Scf\App as ScfApp;
+use Teddy\Swoole\App as SwooleApp;
 
 class AppFactory
 {
     public static function create(string $basePath, string $envFile = '.env')
     {
-        if (defined('IN_SCF') && IN_SCF) {
+        $runtime = defined('TEDDY_RUNTIME') ? TEDDY_RUNTIME : self::fetchRuntime();
+        if ($runtime === 'scf') {
             return self::createScfApp($basePath, $envFile);
         } else {
             return self::createSwooleApp($basePath, $envFile);
@@ -30,5 +31,16 @@ class AppFactory
     public static function createSwooleApp(string $basePath, string $envFile = '.env')
     {
         return new SwooleApp($basePath, $envFile);
+    }
+
+    protected static function fetchRuntime(): string
+    {
+        $runtime = 'swoole';
+        $scf = getenv('TENCENTCLOUD_RUNENV');
+        if ($scf && strtolower($scf) === 'scf') {
+            $runtime = 'scf';
+        }
+
+        return $runtime;
     }
 }
