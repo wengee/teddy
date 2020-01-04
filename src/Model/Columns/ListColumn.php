7c33d136045dc1b260f4b43d8da5359f6d15f7f3
Fung Wing Kit <fengyongjie@vivo.com>
2020-01-04 14:56:49 +0800

    commit

--- src/Model/Columns/ListColumn.php
+++ src/Model/Columns/ListColumn.php
@@ -3,10 +3,12 @@
  * This file is part of Teddy Framework.
  *
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2019-09-24 11:12:51 +0800
+ * @version  2020-01-04 14:56:44 +0800
  */
 
 namespace Teddy\Model\Columns;
+
+use Teddy\Filter;
 
 /**
  * @Annotation
@@ -16,6 +18,8 @@ class ListColumn extends Column
 {
     protected $separator = '|';
 
+    protected $filter = null;
+
     public function dbValue($value)
     {
         return is_array($value) ? implode($this->separator, $value) : '';
@@ -24,7 +28,15 @@ class ListColumn extends Column
     public function value($value)
     {
         if (is_string($value) && strlen($value) > 0) {
-            return explode($this->separator, $value);
+            $arr = explode($this->separator, $value);
+
+            if ($this->filter) {
+                $arr = array_map(function ($item) {
+                    return Filter::instance()->sanitize($item, $this->filter);
+                }, $arr);
+            }
+
+            return $arr;
         }
 
         return [];
