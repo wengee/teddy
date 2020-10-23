683e8d890e73f8ae6a81d81cb935b32a32678caa
Fung Wing Kit <wengee@gmail.com>
2020-10-23 15:03:17 +0800

    更新手机号码正则

--- src/Validation/Validators/Mobile.php
+++ src/Validation/Validators/Mobile.php
@@ -10,7 +10,7 @@ namespace Teddy\Validation\Validators;
 
 class Mobile extends ValidatorRuleBase
 {
-    public const REGEX = '/^1(([3][0-9])|([4][5-9])|([5][0-3,5-9])|([6][5,6])|([7][0-8])|([8][0-9])|([9][1,8,9]))[0-9]{8}$/';
+    public const REGEX = '/^1[3|4|5|6|7|8|9][0-9]{9}$/';
 
     protected $message = ':label不是一个合法的手机号码';
 
