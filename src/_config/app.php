28f78d3579b5ad998fb6f72149d6478d936f64ac
Fung Wing Kit <wengee@gmail.com>
2023-04-24 10:17:10 +0800

    commit: 2023-04-24 10:17:10

--- src/_config/app.php
+++ src/_config/app.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
  * This file is part of Teddy Framework.
  *
  * @author   Fung Wing Kit <wengee@gmail.com>
- * @version  2022-11-09 22:55:40 +0800
+ * @version  2023-04-24 10:15:58 +0800
  */
 
 return [
@@ -12,5 +12,6 @@ return [
     'version'  => '1.0.0',
     'timezone' => 'Asia/Shanghai',
 
+    'baseUrl'   => '',
     'urlPrefix' => '',
 ];
